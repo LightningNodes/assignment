39c36d4570c5f08ef9b9a08f0dbd14d2ffc253f0
git-0r <rohitdec97@gmail.com>
2024-05-04 13:03:38 +0530

    chore: setup project and style lib

---
+++ answers/rohit/app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <p>
+      No one shall be subjected to arbitrary arrest, detention or exile.
+      Everyone is entitled in full equality to a fair and public hearing by an
+      independent and impartial tribunal, in the determination of his rights and
+      obligations and of any criminal charge against him. No one shall be
+      subjected to arbitrary interference with his privacy, family, home or
+      correspondence, nor to attacks upon his honour and reputation. Everyone
+      has the right to the protection of the law against such interference or
+      attacks.
+    </p>
+  );
+}
