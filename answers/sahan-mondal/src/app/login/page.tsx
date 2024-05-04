f881a9cea28e70e3d55ab142a1ee30cfc4bdbdeb
sahanmndl <mondalsahan053@gmail.com>
2024-05-04 18:06:08 +0530

    build error

--- answers/sahan-mondal/src/app/login/page.tsx
+++ answers/sahan-mondal/src/app/login/page.tsx
@@ -35,6 +35,10 @@ const Login = () => {
             })
     };
 
+    if (typeof window === 'undefined') {
+        return null;
+    }
+
     return (
         <div className="flex justify-center items-center h-screen bg-slate-50">
             <form onSubmit={handleSubmit} className="w-full max-w-sm">
--- answers/sahan-mondal/src/app/register/page.tsx
+++ answers/sahan-mondal/src/app/register/page.tsx
@@ -42,6 +42,10 @@ const Register = () => {
         }
     };
 
+    if (typeof window === 'undefined') {
+        return null;
+    }
+
     return (
         <div className="flex justify-center items-center h-screen bg-slate-50">
             <form onSubmit={handleSubmit} className="w-full max-w-sm">
